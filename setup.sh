c280596c115ad5d80eecc8cb25843c6d00f19040
chez1999 <wkbrenton@gmail.com>
2023-02-10 10:56:48 -0700

    added setup.sh and requirments.txt

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# download python 3.10
+sudo apt-get update
+sudo apt-get install python3.10
+
+# create venv and enter it
+python3.10 -m venv jax_venv
+source jax_venv/bin/activate
+
+# install jax[tpu]
+pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+
+# install requirments.txt
+pip install -r requirmens
